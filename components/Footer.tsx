3269e8674a680cca2fba1b8f205524dc48d52b3c
GoutamK1902 <goutamkhowal321@gmail.com>
2026-08-21 03:18:39 +0530

    icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -110,7 +110,7 @@ export default function Footer() {
           <div className="footer-brand-block md:col-span-2 space-y-4">
             <div className="flex items-center gap-3">
               <Image
-                src="/icon.png"
+                src="/icon1.png"
                 alt="A&B Construction Logo"
                 width={38}
                 height={30}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -216,7 +216,7 @@ export default function Navbar() {
             className="flex items-center gap-3 group min-h-[48px] focus:outline-none focus:ring-2 focus:ring-amber-500 rounded-sm p-1"
           >
             <Image
-              src="/icon.png"
+              src="/icon1.png"
               alt="A&B Construction Logo"
               width={38}
               height={30}
